9bc4a8f57c6c8fc5e4176767f21025c049e37e6b
Margarida <margarida97pt@gmail.com>
2021-03-23 20:32:05 +0000

    finishing touches

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-const camera = document.querySelector('.camera');
+const animation = document.querySelector(".animation");
 
-camera.addEventListener.addEventListener('mousemove', (e) => {
-    let xAxis = (window.innerWidth / 2 - e.pageX);
-    let yAxis = (window.innerHeight / 2 - e.pageY);
-    camera.style.transform = `rotateY(${yAxis}deg) rotateX(${xAxis}deg)`;
+
+
+animation.addEventListener("mousemove", (e) => {
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 10;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 10;
+  animation.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
+
+//Animate In
+animation.addEventListener('mouseenter', (e) => {
+   animation.style.transition = 'none';
+});
+
+
+//Animate Out
+animation.addEventListener('mouseleave', (e) => {
+    animation.style.transition = "0.5s ease";
+    animation.style.transform = `rotateY(0deg) rotateX(0deg)`;
 });
